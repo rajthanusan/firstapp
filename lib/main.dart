e9f40db99cc55cb8667290c3f4256e292f0007c8
rajthanusan <rajthanusan08@gmail.com>
2023-10-18 22:55:02 +0530

    update2

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,63 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(new MaterialApp(
-    home: new MyApp(),
-  ));
-}
+void main() => runApp(new MaterialApp(
+      home: new MyApp(),
+    ));
 
 class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
   @override
-  _MyAppState createState() => _MyAppState();
+  State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('hi thanusan'),
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            print('Icon Button Click');
+          },
+        ),
+        title: Text('Appbar Demo'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: () {},
+          )
+        ],
+        flexibleSpace: SafeArea(
+          child: Icon(
+            Icons.camera,
+            color: Colors.white,
+            size: 30.0,
+          ),
+        ),
+        bottom: PreferredSize(
+          child: Container(
+            color: Colors.grey,
+            height: 75.0,
+            width: double.infinity,
+            child: Text(
+              'Title',
+              style: TextStyle(color: Colors.white, fontSize: 30.0),
+            ),
+          ),
+          preferredSize: Size.fromHeight(75.0),
+        ),
       ),
-      body: new Container(
-        child: new Text('welcome to flutter'),
+      body: Center(
+        child: Text(
+          'thanusan',
+          style: TextStyle(fontSize: 28.0, color: Colors.red),
+        ),
       ),
     );
   }
